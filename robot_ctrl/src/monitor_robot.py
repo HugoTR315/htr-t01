f4a9178eaf6a5e61f3f0dd24a2f7b443f44a438e
HugoTR315 <hugo.torres.rocha.315347@gmail.com>
2021-11-18 16:23:58 -0600

    Archivos de clase

--- robot_ctrl/src/monitor_robot.py
+++ robot_ctrl/src/monitor_robot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import rospy
+from geometry_msgs.msg import Point
+from nav_msgs.msg import Odometry
 from gazebo_msgs.srv import GetWorldProperties, GetModelState
+from robot_ctrl.srv import GetClosest, GetClosestResponse, GetDistance, GetDistanceResponse
 
-class GazaboUtils(object):
+class GazeboUtils(object):
     def __init__(self) -> None:
         pass
     def getWorldProperties(self):
@@ -37,15 +40,54 @@ class GazaboUtils(object):
         else:
             return None
 
-# class DisntanceMonitor():
+
+class DisntanceMonitor():
+    def __init__(self):
+        self._landmarks ={}
+        self._exclude = ['ground_plane','turtlebot3_waffle']
+        self._gazebo_utils = GazeboUtils()
+        self._position = Point()
+        self._odom_sub = rospy.Subscriber('/odom',Odometry, self._odom_callback)
+        # Hacemos los servicios
+        self._getClosestSvr = rospy.Service('/get_closest',GetClosest,self.get_closest_srv)
+        self._getDistanceSrv = rospy.Service('/get_distance',GetDistance,self.get_distance_srv)
+
+    def _init(self):
+        wp = self._gazebo_utils.getWorldProperties()
+        if wp:
+            for model in wp.model_names():
+                if model not in self._exclude:
+                    ms = self._gazebo_utils.getModelState(model)
+                    position = (ms.position.x,ms.position.y) # Generamos la tupla de las posiciones
+                    self._landmarks.update({model: position})
+            rospy.loginfo('{} landmarks encontradas. '.format(len(self._landmarks)))
+
+    def _odom_callback(self,msg):
+        self._position = msg.pose.pose.position
+
+    def get_closest_srv(self):
+        rospy.loginfo('GetClosest llamado')
+        closest_landmark = ''
+        closest_distance = -1
+        for model_name, (x,y) in self._landmarks.items():
+            dx = x - self._position.x
+            dy = y - self._position.y
+            sq_dist = (dx * dx) + (dy * dy)
+            if closest_distance == -1 or sq_dist < closest_distance:
+                closest_distance = sq_dist
+                closest_landmark = model_name
+        
+        response = GetClosestResponse()
+        response.name = closest_landmark
+
+        return response
     
-
-
-
+    def get_distance_srv(self):
+        pass
 
 
 def test_services():
-    gazebo_utils=GazaboUtils()
+    gazebo_utils = GazeboUtils()
     exclude = ['ground_plane','turtlebot3_waffle']
     landmarks = {}
     print('Probando GazeboUtils, get_world_properties service:')
