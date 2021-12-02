4088c10a07ea7d0998bd4b99f64d3099e7cc27bb
HugoTR315 <hugo.torres.rocha.315347@gmail.com>
2021-12-02 17:17:29 -0600

    Archivos modificados

---
+++ battery_simulator/src/battery_sim_client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import actionlib
+from battery_simulator.msg import Battery_SimAction, Battery_SimGoal
+
+
+def battery_state(charge_condition):
+    goal = Battery_SimGoal()
+    goal.charge_state = charge_condition
+    client.send_goal(goal)
+
+
+if __name__ == '__main__':
+    rospy.init_node('BatterySimClient')
+    client = actionlib.SimpleActionClient('battery_simulator',Battery_SimAction)
+    client.wait_for_server()
+    #param = int(sys.argv[1]) #Rescatamos el primer argumento
+    print('recibi param: %s', param)
+    battery_state(param)
+    client.wait_for_result()
--- battery_simulator/src/battery_sim_server.py
+++ battery_simulator/src/battery_sim_server.py
@@ -4,10 +4,30 @@ import rospy
 import time
 from multiprocessing import Process, process
 import actionlib
-from battery_simulator.msg import Battery_SimAction, Battery_SimGoal
+from battery_simulator.msg import Battery_SimAction, Battery_SimGoal, Battery_SimResult, Battery_SimFeedback
 
 def batterySim():
-    pass
+    battery_lavel = 100
+    result = Battery_SimResult()
+    while not rospy.is_shutdown():
+        if rospy.has_param('/battery_sim/BatteryStatus'):
+            time.sleep(1)
+            param = rospy.get_param('/battery_sim/BatteryStatus')
+            if param == 1:
+                if battery_lavel == 100:
+                    result.battery_status = 'Cargada'
+                    server.set_succeeded(result)
+                    break
+                else:
+                    battery_lavel +=1
+                    result.battery_status = 'Cargando'
+                    rospy.loginfo('Cargando... actualmente %s', battery_lavel)
+                    time.sleep(4)
+            elif param == 0:
+                battery_lavel -= 1
+                rospy.logwarn('Descargando... Actualmente %s', battery_lavel)
+                time.sleep(2)
+
 
 def goal_clbck(goal):
     rate = rospy.Rate(2) # Relog de ROS
@@ -19,7 +39,8 @@ def goal_clbck(goal):
     elif goal.charge_state == 1:
         rospy.set_param('/battery_sim/BatteryStatus',goal.charge_state)
         
-
 if __name__ == '__main__':
     rospy.init_node('BatterySimServer')
     server = actionlib.SimpleActionServer('battery_simulator',Battery_SimAction,goal_clbck,False)
+    server.start()
+    rospy.spin()
